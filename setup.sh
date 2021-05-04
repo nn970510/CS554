808ad9620a52ce4d27bb42cd0d5db57f719062c5
nn970510 <31712072+nn970510@users.noreply.github.com>
2021-05-03 21:27:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"nn70510@gmail.com\"\n\
+email = \"nn970510@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS = false\n\
-port = 10000\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
